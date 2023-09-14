6898aa8e694707e762f227a600bc068c6b4a56cd
Ryan Rodrigo <ryanr4057@gmail.com>
2023-09-14 14:42:09 -0300

    Organização do Código

--- app_editais/models.py
+++ app_editais/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Aluno(models.Model):
     matricula = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=100)
--- app_editais/urls.py
+++ app_editais/urls.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from app_editais import views
-
 import app_editais
 
 urlpatterns = [
-    path('', views.home,name='home'),
-    path('prex/create', views.prex,name='prex'),
-    path('edital/criar', views.edital_criar, name='edital_criar'),
+    # path('', views.home,name='home'),
+    path('prex', views.prex, name='prex'),
+    path('prex/cadastro', views.prex_cadastro,name='prex_cadastro'),
+    path('prex/message', views.prex_message, name='prex_message'),
     path('aluno', views.aluno_home, name='aluno_home'),
     path('aluno/cadastro', views.aluno_cadastro, name='aluno_cadastro'),
     path('aluno/message', views.aluno_message, name='aluno_message'),
+    path('edital/criar', views.edital_criar, name='edital_criar'),
     path('edital/message', views.edital_message, name='edital_message'),
-    path('criar_edital', views.create_edital, name='edital_criar')
+    path('logout', views.logout_view, name='logout'),
 ]
+
